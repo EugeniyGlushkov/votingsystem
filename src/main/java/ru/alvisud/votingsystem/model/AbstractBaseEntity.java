eef2680697a15deba880852480a64d41cda7e4a6
EugeniyGlushkov <eugeniyglushkov@gmail.com>
2018-02-10 23:03:37 +0300

    add_DB

--- src/main/java/ru/alvisud/votingsystem/model/AbstractBaseEntity.java
+++ src/main/java/ru/alvisud/votingsystem/model/AbstractBaseEntity.java
@@ -6,8 +6,6 @@ import ru.alvisud.votingsystem.HasId;
 import javax.persistence.*;
 
 @MappedSuperclass
-
-// http://stackoverflow.com/questions/594597/hibernate-annotations-which-is-better-field-or-property-access
 @Access(AccessType.FIELD)
 //@JsonAutoDetect(fieldVisibility = ANY, getterVisibility = NONE, isGetterVisibility = NONE, setterVisibility = NONE)
 public abstract class AbstractBaseEntity implements HasId {
