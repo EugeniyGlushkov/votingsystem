5462d3c7a4c4ac0ff19081d39d86fb2a9c0bda3a
EugeniyGlushkov <eugeniyglushkov@gmail.com>
2018-08-29 12:17:46 +0300

    clean main

--- src/main/java/ru/alvisid/votingsystem/Main.java
+++ src/main/java/ru/alvisid/votingsystem/Main.java
@@ -14,30 +14,4 @@ import java.util.concurrent.TimeUnit;
 
 //del
 public class Main {
-    private static final Logger log = LoggerFactory.getLogger(Main.class);
-
-    public static void main(String[] args) {
-        float weight = 1; //вес кирпча
-        int row = 5;      //номер ряда начиная с 1 с верху
-        int num = 3;      //номер кирпича слева (или с права, как больше нравится) с 1
-        System.out.println(pressueOnBrick(row, num, weight));
-    }
-
-    //давление на один кирпич
-    public static float pressueOnBrick(int row, int num, float weight) {
-        return pressue(row, num, weight) - weight;  //давление оказываемое на один кирпич = полное
-                                                    //давление которое оказывает кирпич минус вес кирпича
-    }
-
-    //полное давление оказываемое кирпичём (включая свой вес)
-    public static float pressue(int row, int num, float weight) {
-        //в ряду не может быть кирпичей с номером меньше 1 и больше чем номер ряда(число кирпичей в ряду = номеру ряда)
-        if (num < 1 || num > row) {
-            return 0;
-        }
-
-        //давление которое оказывает кирпич равно сумме веса кирпича и половине веса кирпичей которые давят на него
-        //слева и справа
-        return weight + (pressue(row - 1, num - 1, weight) + pressue(row - 1, num, weight)) / 2;
-    }
 }
